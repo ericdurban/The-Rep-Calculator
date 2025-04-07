f3e3828c66988dfe5d68232873b02be0450a9353
Eric Urban <ericdurban32@gmail.com>
2025-04-07 15:31:33 -0700

    Update to browserrouter for react app in github pages to render.

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </StrictMode>
 );
