a2d6002c87b6bf0f209667fb190703b704e51284
Eric Urban <ericdurban32@gmail.com>
2025-04-07 16:31:00 -0700

    test#6 for github pages.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import {HashRouter, Route, Routes} from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/The-Rep-Calculator">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
