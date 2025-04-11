fa78bf02db498560e665f544fe213ad7dbaa729f
Eric Urban <ericdurban32@gmail.com>
2025-04-11 12:11:45 -0700

    github test #9

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import {HashRouter, Route, Routes} from 'react-router-dom';
+// import {HashRouter, Route, Routes} from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    {/* <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
-    </HashRouter>
+    </HashRouter> */}
+    <App />
   </StrictMode>
 );
