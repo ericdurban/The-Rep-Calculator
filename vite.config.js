6cbb9b3f6cfe065513835ea2a81c88a51956b590
Eric Urban <ericdurban32@gmail.com>
2025-04-15 08:10:07 -0700

    update vite.config.js with webcrypto patch.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
+import { webcrypto } from 'crypto';
+
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto;
+}
 
 export default defineConfig(({ mode }) => ({
   plugins: [
