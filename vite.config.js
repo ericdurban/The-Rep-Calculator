59ee0522bbb6d9110687824ce7eaf678fef9ff32
Eric Urban <ericdurban32@gmail.com>
2025-04-14 17:11:20 -0700

    update import for NodePolyFill

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills-node';
 
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
-    nodePolyfills({
-      crypto: true,
-    }),
   ],
   base: mode === 'production' ? '/The-rep-calculator/' : '/',
   resolve: {
     alias: {
         '@': '/src',
+    },
+  },
+  optimizeDeps: {
+    include: ['crypto'],
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill(),
+      ],
     },
   },
 }));
